f9c1e896bc6915440bb1fce5c09e0ba38ead1a37
Lukas Bableck <lukas@bableck.dev>
2024-09-10 09:18:03 +0200

    add backend script if dca contains required fields

--- src/EventListener/LoadDataContainerListener.php
+++ src/EventListener/LoadDataContainerListener.php
@@ -2,15 +2,22 @@
 namespace Lukasbableck\ContaoTitleDescriptionBundle\EventListener;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 #[AsHook('loadDataContainer')]
 class LoadDataContainerListener {
+	public function __construct(private RequestStack $requestStack, private ScopeMatcher $scopeMatcher) {
+	}
+
 	public function __invoke(string $table): void {
-		if (\array_key_exists('pageTitle', $GLOBALS['TL_DCA'][$table]['fields'])) {
-			$GLOBALS['TL_DCA'][$table]['fields']['pageTitle']['wizard'] = [WizardListener::class, '__invoke'];
+		$request = $this->requestStack->getCurrentRequest();
+		if (null == $request || !$this->scopeMatcher->isBackendRequest($request) || 'edit' !== $request->query->get('act')) {
+			return;
 		}
-		if (\array_key_exists('description', $GLOBALS['TL_DCA'][$table]['fields'])) {
-			$GLOBALS['TL_DCA'][$table]['fields']['description']['wizard'] = [WizardListener::class, '__invoke'];
+		if (\array_key_exists('pageTitle', $GLOBALS['TL_DCA'][$table]['fields'])
+			&& \array_key_exists('description', $GLOBALS['TL_DCA'][$table]['fields'])) {
+			$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaotitledescription/backend.js';
 		}
 	}
 }
